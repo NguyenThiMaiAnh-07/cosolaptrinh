7d25bdd218c8f4943e08cc13b78ac42fc29c0865
Mai Anh <maianh21102004@gmail.com>
2023-03-05 11:08:49 +0700

    add exercise 3.2

--- Chuong3/Bai3.2.py
+++ Chuong3/Bai3.2.py
@@ -1,7 +1,12 @@
-n=int(input("Nhap so nguyen: (n>=1)"))
-i=1
-S=0
-while i<=n:
-     S=S+i
-     i=i+1
-print("Tong S=",S)
+S=int(input("S="))
+M1=int(input("M1="))
+M2=int(input("M2="))
+M3=int(input("M3="))
+if S<=100:
+     TienDien=S*M1
+elif S<=150:
+     TienDien=100*M1+(S-100)*M2
+elif S>=151:
+     TienDien=100*M1+50*M2+(S-150)*M3
+print("Phai tra=",TienDien,sep="")
+     
