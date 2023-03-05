93a6859579500e5e72153fe035790c0828b79c23
Mai Anh <maianh21102004@gmail.com>
2023-03-05 22:44:44 +0700

    Exercise 3.3

--- Chuong3/Bai3.3.py
+++ Chuong3/Bai3.3.py
@@ -1,6 +1,17 @@
-while True:
-     n=int(input("n="))
-     if n<=0:
-         print("Khong hop le!!!\nMoi nhap lai")
-     else:
-         break
+x=float(input("x="))
+y=float(input("y="))
+if x+y: 
+    print("Phep toan:+\n" + str(x)+ "+" + str(y) + "=",(x+y))
+elif x-y:
+    print("Phep toan:-\nx-y=",x-y)
+elif  x*y:
+    print("Phep toan:*\nx*y=",x*y)
+elif x/y:
+    print("Phep toan:/\nx/y=",x/y)
+elif x/y and y==0:
+    print("Khong hop le",y=0)
+else:
+    print("Khong hop le")
+    
+
+    
