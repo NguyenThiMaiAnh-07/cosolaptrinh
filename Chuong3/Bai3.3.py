2019a682f97d68a0ac9b0212b77b30b59c914633
NguyenThiMaiAnh-07 <126187305+NguyenThiMaiAnh-07@users.noreply.github.com>
2023-03-06 23:56:31 +0700

    Update Bai3.3.py

--- Chuong3/Bai3.3.py
+++ Chuong3/Bai3.3.py
@@ -1,13 +1,15 @@
 x=float(input("x="))
 y=float(input("y="))
-if x+y: 
-    print("Phep toan:+\n" + str(x)+ "+" + str(y) + "=",(x+y))
-elif x-y:
-    print("Phep toan:-\nx-y=",x-y)
-elif  x*y:
-    print("Phep toan:*\nx*y=",x*y)
-elif x/y:
-    print("Phep toan:/\nx/y=",x/y)
+ch=str(input("Phep toan:"))
+
+if ch=="+": 
+    print(x+y)
+elif ch=="-":
+    print(x-y)
+elif  ch=="*":
+    print(x*y)
+elif ch=="/":
+    print(x/y)
 elif x/y and y==0:
     print("Khong hop le",y=0)
 else:
